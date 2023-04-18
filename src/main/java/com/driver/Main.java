a692ecabf006a03c370e5c80a332d7fc75be5ecb
vasu <pentapativasu3333@gmail.com>
2023-04-18 21:46:07 +0530

    done till task 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,20 +3,20 @@ package com.driver;
 
 public class Main {
   public static class A{
-    public String method(){
+    public String meth(){
       return "Invoking method from class A";
     }
   }
   public static class B extends A{
     @Override
-    public String method(){
+    public String meth(){
       return "Method is overridden in Extendend class B";
     }
   }
 
   public static void main(){
     B obj=new B();
-   obj.method();
+   obj.meth();
   }
 
 }
